8184f6a6efeb473e3c3dd1bc2597c76fb5337255
Jacob Smith <129335957+jacobsmithdev@users.noreply.github.com>
2025-10-02 11:28:45 -0600

    feat: Add search bar to get weather data for a location
    Add a search bar which tries to find weather data for a location. If no
    data can be found, display a message in the dom - otherwise, display the
    data as a JSON string.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import './styles.css';
 
-console.log('hello world');
+import { getWeatherData } from './modules/weatherAPI';
+
+const locationForm = document.querySelector('#location-search-form');
+const locationInput = document.querySelector('#location-search');
+const dataDiv = document.querySelector('#weather-data');
+const locationErrorBox = document.querySelector('#location-search-error');
+
+locationForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    locationErrorBox.textContent = '';
+
+    const location = locationInput.value;
+    if (!location) return;
+
+    const data = await getWeatherData(location);
+    if (!data) {
+        locationErrorBox.textContent = 'Could not find data for that location.';
+        dataDiv.textContent = '';
+        return;
+    }
+    dataDiv.textContent = JSON.stringify(data);
+});
